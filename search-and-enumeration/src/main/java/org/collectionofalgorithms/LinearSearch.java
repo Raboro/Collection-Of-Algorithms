67b7fce80d0b77e64200f129383ab80501c7243d
Raboro <marius.woerfelreal@gmail.com>
2022-11-11 14:49:41 +0100

    added Javadoc for methods

--- search-and-enumeration/src/main/java/org/collectionofalgorithms/LinearSearch.java
+++ search-and-enumeration/src/main/java/org/collectionofalgorithms/LinearSearch.java
@@ -5,6 +5,13 @@ import java.util.Optional;
 
 public class LinearSearch {
 
+    /**
+     * Searched element must be same Datatype as Iterator holds
+     *
+     * @param list     Insert any class, which implements Iterator as Iterator
+     * @param searched Insert element to be searched for
+     * @return searched element or null
+     */
     public static <T> T searchForElement(Iterator<T> list, T searched) {
         while (list.hasNext()) {
             T element = list.next();
@@ -15,6 +22,13 @@ public class LinearSearch {
         return null;
     }
 
+    /**
+     * Searched element must be same Datatype as Iterator holds
+     *
+     * @param list     Insert any class, which implements Iterator as Iterator
+     * @param searched Insert element to be searched for
+     * @return Optional of searched element
+     */
     public static <T> Optional<T> searchForOptional(Iterator<T> list, T searched) {
         while (list.hasNext()) {
             T element = list.next();
@@ -25,6 +39,13 @@ public class LinearSearch {
         return Optional.empty();
     }
 
+    /**
+     * Searched element must be same Datatype as Iterator holds
+     *
+     * @param list     Insert any class, which implements Iterator as Iterator
+     * @param searched Insert element to be searched for
+     * @return searched element index or Zero
+     */
     public static <T> int searchForElementIndex(Iterator<T> list, T searched) {
         int index = 0;
         while (list.hasNext()) {
@@ -37,6 +58,13 @@ public class LinearSearch {
         return 0;
     }
 
+    /**
+     * Searched element must be same Datatype as Iterator holds
+     *
+     * @param list     Insert any class, which implements Iterator as Iterator
+     * @param searched Insert element to be searched for
+     * @return Optional of searched element index
+     */
     public static <T> Optional<Integer> searchForOptionalIndex(Iterator<T> list, T searched) {
         int index = 0;
         while (list.hasNext()) {
